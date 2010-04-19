c1e84243583479193caee112497c4c9b39f2a829
gapan <gapan@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-04-19 21:42:22 +0000

    Added arch option to 01 script

--- 01-downloadpackages.sh
+++ 01-downloadpackages.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+
 export LANG=en_US
-slapt-get -u
+export ARCH=${ARCH:-i486}
+
+slapt-get -u -c slapt-getrc.$ARCH
 slapt-get --clean
 {
 	for i in `cat lists/KERNEL lists/CORE lists/BASIC lists/FULL lists/SETTINGS`; do 
-	slapt-get -d --no-dep --reinstall -i $i
+	slapt-get -d --no-dep --reinstall -c slapt-getrc.$ARCH -i $i
 	done
-} 2>&1 | tee download.log
+} 2>&1 | tee download-$ARCH.log
 
-grep "connect to server" download.log
+grep "connect to server" download-$ARCH .log
