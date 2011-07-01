24e50aaa5901d9ec74b4160f11a90f877f7b716d
djemos <djemos@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2011-07-01 19:14:55 +0000

    corrected 06-cleanup.sh

--- 06-cleanup.sh
+++ 06-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-find ./salix -name *.dep -exec rm {} \;
-find ./salix -name *.txt -exec rm {} \;
-find ./salix -name *.meta -exec rm {} \;
+find ./salix -name "*.dep" -exec rm {} \;
+find ./salix -name "*.txt" -exec rm {} \;
+find ./salix -name "*.meta" -exec rm {} \;
 
 unlink lists
