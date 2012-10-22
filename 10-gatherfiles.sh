2f4f17ada4b181883a92f6c894150fa405d50e28
George Vlahavas <vlahavas@gmail.com>
2012-10-22 18:16:58 +0300

    Remove the nonsmp kernel and initrd from iso
    If the nonsmp kernel is not specifically requested, remove the kernel
    from the iso as well as the corresponding initrd.

--- 10-gatherfiles.sh
+++ 10-gatherfiles.sh
@@ -6,11 +6,12 @@ if [ "$UID" -eq "0" ]; then
 fi
 
 if [ ! $# -eq 1 ]; then
-	echo "ERROR. Syntax is: $0 ARCH"
+	echo "ERROR. Syntax is: $0 ARCH [both_smp_nonsmp]"
 	exit 1
 fi
 
 arch=$1
+smp=$2
 
 mkdir -p iso/isolinux
 mkdir -p iso/kernels
@@ -18,3 +19,14 @@ mkdir -p iso/kernels
 cp -r isolinux/$arch/* iso/isolinux/
 cp -r kernel/$arch/* iso/kernels/
 cp README iso/
+
+# remove the non-smp initrd and kernel files if building an smp-only iso
+if [ $arch == "i486" ]; then
+	if [ $smp != "both_smp_nonsmp" ]; then
+		sed -i "/huge\.s/d" iso/isolinux/isolinux.cfg
+		sed -i "/huge\.s/d" iso/isolinux/message.txt
+		sed -i "/Pentium-pro/d" iso/isolinux/message.txt
+		rm iso/isolinux/nonsmp.img
+		rm -r iso/kernels/huge.s
+	fi
+fi
