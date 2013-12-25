9728b232acb431a7f1bffdfd8ee04fb656c0d765
George Vlahavas <vlahavas@gmail.com>
2013-12-25 21:49:28 +0200

    Use dialog in 10-gatherfiles.sh

--- 10-gatherfiles.sh
+++ 10-gatherfiles.sh
@@ -5,15 +5,36 @@ if [ "$UID" -eq "0" ]; then
 	exit 1
 fi
 
-if [ ! $# -eq 1 ]; then
-	if [ ! $# -eq 2 ]; then
-		echo "ERROR. Syntax is: $0 ARCH [both_smp_nosmp]"
-	fi
-	exit 1
+answer="$(eval dialog --title \"Select arch\" \
+	--stdout \
+	--menu \"Select the target architecture:\" \
+	0 0 0 \
+	'i486' 'o' \
+	'x86_64' 'o')"
+retval=$?
+if [ $retval -eq 1 ] || [ $retval -eq 255 ]; then
+	exit 0
+else
+	arch=$answer
 fi
 
-arch=$1
-smp=$2
+smp=0
+if [ $arch == "i486" ]; then
+	answer="$(eval dialog --title \"Include i486 non-SMP kernel?\" \
+	--stdout \
+	--menu \"Do you want to include the i486 non-SMP kernel?\" \
+	0 0 0 \
+	'NO' 'o' \
+	'Yes' 'o' )"
+	retval=$?
+	if [ $retval -eq 1 ] || [ $retval -eq 255 ]; then
+		exit 0
+	else
+		if [ "$answer" == "Yes" ]; then
+			smp=1
+		fi
+	fi
+fi
 
 mkdir -p iso/isolinux
 mkdir -p iso/kernels
@@ -24,7 +45,7 @@ cp README iso/
 
 # remove the non-smp initrd and kernel files if building an smp-only iso
 if [ $arch == "i486" ]; then
-	if [ x"$smp" != x"both_smp_nosmp" ]; then
+	if [ $smp -eq 0 ]; then
 		sed -i "/huge\.s/d" iso/isolinux/isolinux.cfg
 		sed -i "/f2\.txt/d" iso/isolinux/isolinux.cfg
 		sed -i "/huge\.s/d" iso/isolinux/message.txt
