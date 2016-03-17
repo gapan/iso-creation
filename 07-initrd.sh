8fbdf5f94a76d7f46d585f336795a95be47eb4de
George Vlahavas <vlahavas@gmail.com>
2016-03-18 00:04:39 +0200

    Update 07-initrd.sh
    - Add support for different locales
    - Update repo version numbers
    - Use only xzdec from the xz package
    - Remove installation of packages not needed anymore
    - Add nano to the iso
    - Add fonts needed for translations
    - Add locale files
    - Don't run depmod as we're not actually adding any modules anymore

--- 07-initrd.sh
+++ 07-initrd.sh
@@ -18,6 +18,9 @@ set -e
 CWD=`pwd`
 SCRIPTSDIR=$CWD/initrd-scripts
 
+# The script SeTlocales contains functions and variable settings - Didier
+. $SCRIPTSDIR/usr-lib-setup/SeTlocales
+
 if [ "$UID" != "0" ]; then
 	echo "You need to be root to run this"
 	exit 1
@@ -61,7 +64,7 @@ installed.\n\
 You need to be running a stock slackware kernel.\n\
 \n\
 You also need to be running the architecture for which you are\n\
-creating the initrd for. The architecture you are running now\n\
+creating the initrd. The architecture you are running now\n\
 is ${arch}.\n\
 \n\
 ARE YOU SURE YOU KNOW WHAT YOU'RE DOING?"
@@ -79,10 +82,14 @@ if [[ "$arch" == "x86_64" ]]; then
 	export LIBDIRSUFFIX="64"
 fi
 
-REPO=http://slackware.org.uk
-SLACKREPO=$REPO/slackware/slackware${LIBDIRSUFFIX}-$VER
+REPO=http://slackware.uk
+
+# http://slackware.uk/slackware/slackware64-14.2/
+SLACKREPO=$REPO/slackware/slackware${LIBDIRSUFFIX}-current
 SLACK2REPO=$SLACKREPO/slackware${LIBDIRSUFFIX}
-SALIXREPO=$REPO/salix/$arch/$VER/
+# http://slackware.uk/slackware/slackware64-14.2/slackware64/
+SALIXREPO=$REPO/salix/$arch/14.2/
+# http://slackware.uk/salix/x86_64/14.2/
 
 mkdir -p initrd/$arch
 rm -rf initrd/$arch/*initrd*.img
@@ -91,6 +98,7 @@ rm -rf initrd/$arch/*initrd*.img
 # use wget instead of ftp, less error prone
 echo "Getting the slackware initrd..."
 wget -q $SLACKREPO/isolinux/initrd.img -O initrd/$arch/slack-initrd.img
+
 
 # unpack slack initrd
 echo "Unpacking slackware initrd..."
@@ -114,116 +122,112 @@ cp $SCRIPTSDIR/usr-lib-setup/* /boot/initrd-tree/usr/lib/setup/
 # download and install required packages
 rm -f slack.md5 salix.md5
 echo "Downloading slack CHECKSUMS.md5 file"
-wget -q $SLACK2REPO/CHECKSUMS.md5 -O slack.md5
+wget -q $SLACKREPO/CHECKSUMS.md5 -O slack.md5
+grep " xz-*t?z" slack.md5
 echo "Downloading salix CHECKSUMS.md5 file"
 wget -q $SALIXREPO/CHECKSUMS.md5 -O salix.md5
 
 echo "Downloading spkg..."
-rm -f spkg-*.txz
+rm -f spkg-*.t?z
 LOC=`grep "\/spkg-.*-.*-.*\.tgz$" salix.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
 wget -q $SALIXREPO/$LOC
 echo "Installing spkg..."
-spkg -qq --root=/boot/initrd-tree/ -i spkg-*.tgz
-rm spkg-*.tgz
+spkg -qq --root=/boot/initrd-tree/ -i spkg-*.t?z
+rm spkg-*.t?z
 
+# We just  need xzdec, needed by spkg. - Didier
+install_xzdec () {
 echo "Downloading xz..."
-rm -f xz-*.tgz
-LOC=`grep "\/xz-.*-.*-.*\.tgz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SLACK2REPO/$LOC
-echo "Installing xz..."
-spkg -qq --root=/boot/initrd-tree/ -i xz-*.tgz
-rm xz-*.tgz
-
-install_nfsutils() {
-echo "Downloading nfsutils..."
-rm -f nfs-utils-*.txz
-LOC=`grep "\/nfs-utils-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SLACK2REPO/$LOC
-echo "Installing showmount binary from nfsutils..."
-tar xf nfs-utils-*.txz -C /boot/initrd-tree usr/sbin/showmount
-rm nfs-utils-*.txz
+rm -f xz-*.t?z
+LOC=`grep "xz-.*t[gx]z$" slack.md5|sed "s|\(.*\)  \./\(.*\)|\2|"`
+wget -q $SLACKREPO/$LOC
+echo "Installing xzdec ..."
+tar xf xz-*.t?z -C /boot/initrd-tree usr/bin/xzdec
+mv /boot/initrd-tree/usr/bin/xzdec /boot/initrd-tree/bin/xzdec
+rm -f xz-*.t?z
 }
-#install_nfsutils
+install_xzdec
 
-install_fuse() {
-echo "Downloading fuse..."
-rm -f fuse-*.txz
-LOC=`grep "\/fuse-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SLACK2REPO/$LOC
-echo "Installing fuse..."
-spkg -qq --root=/boot/initrd-tree/ -i fuse-*.txz
-rm fuse-*.txz
-echo "Downloading kernel modules"
-MODULES=`grep "\/kernel-modules-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-for LOC in $MODULES; do
-	wget -q $SLACK2REPO/$LOC
+# We need gettext (but not gettext-tools) - Didier
+install_gettext() {
+echo "Downloading gettext..."
+rm -rf gettext-*.txz
+LOC=`grep "gettext-[[:digit:]].*.t[gx]z$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
+wget -q $SLACKREPO/$LOC
+tar xf gettext-*t?z -C /boot/initrd-tree --wildcards \
+  usr/bin/{envsubst,gettext*}
+rm -f gettext-*.txz
+}
+install_gettext
+
+# We need appropriate fonts for each locale - Didier
+install_fonts() {
+echo "Installing terminus fonts"
+rm -f terminus-font-*
+LOC=`grep "terminus-font.*t[gx]z$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
+wget -q $SLACKREPO/$LOC
+FONT_LIST=$(grep -o "FONT[^=]*=[^;]*" SeTlocales|sort|uniq|cut -d= -f2|sed ':a;N;s/\n/ /;ta')
+for i in $FONT_LIST; do
+  tar xf terminus-font-*t?z -C /boot/initrd-tree \
+    usr/share/kbd/consolefonts/$i
 done
-echo "Installing fuse.ko kernel modules..."
-for i in `ls kernel-modules-*.txz`; do
-	tar xf $i -C /boot/initrd-tree --wildcards "*/fuse.ko"
+rm -f terminus-font-*
+}
+install_fonts
+
+# We need messages catalogs for available locales - Didier
+install_messages_catalogs() {
+echo "Installing messages catalogs"
+SLINTREPO="http://slint.fr/testing"
+rm -f salix-installer-messages-*z
+# For now (testing) the catalogs are just the Slint ones, renamed
+# salix-installer.mo instead of slint.mo - Didier
+wget -q $SLINTREPO/salix-installer-msg-noarch-1.txz
+spkg -qq -i --root=/boot/initrd-tree/ salix-installer-msg-noarch-1.txz
+rm -f rm -f salix-installer-msg-noarch-1.txz
+}
+install_messages_catalogs
+
+# We need definitions for all available locales. - Didier
+install_locales_definitions() {
+echo "Installing locale definitions"
+mkdir -p /boot/initrd-tree/usr/lib$LIBDIRSUFFIX/locale
+TMPDIR=$(mktemp -d)
+rm -rf glibc-i18n-*.txz
+LOC=`grep "glibc-i18n-.*.t[gx]z$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
+wget -q $SLACKREPO/$LOC
+# We can't cherry pick the locales we need from the archive, because it
+# contains hard links. - Didier
+tar -xf glibc-i18n-*t?z -C $TMPDIR
+# LOCALES is set in SeTlocales, sourced above.
+for i in $LOCALES; do
+  cp -a $TMPDIR/usr/lib$LIBDIRSUFFIX/locale/$i \
+  /boot/initrd-tree/usr/lib$LIBDIRSUFFIX/locale
 done
-rm kernel-modules-*.txz
+rm -rf $TMPDIR
+rm -f glibc-i18n-*z
 }
-#install_fuse
+install_locales_definitions
 
-install_btrfs_progs() {
-echo "Downloading btrfs-progs..."
-rm -f btrfs-progs-*.txz
-LOC=`grep "\/btrfs-progs-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SLACK2REPO/$LOC
-echo "Installing btrfs-progs..."
-spkg -qq --root=/boot/initrd-tree/ -i btrfs-progs-*.txz
-rm btrfs-progs-*.txz
-echo "Downloading kernel modules"
-MODULES=`grep "\/kernel-modules-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-for LOC in $MODULES; do
-	wget -q $SLACK2REPO/$LOC
-done
-echo "Installing btrfs.ko kernel modules..."
-for i in `ls kernel-modules-*.txz`; do
-	tar xf $i -C /boot/initrd-tree --wildcards "*/btrfs.ko"
-done
-rm kernel-modules-*.txz
+# lets'install nano, a newbie friendly text editor, and its dependency
+# libmagic.
+install_nano() {
+echo "Installing nano and its dependency libmagic"
+rm -rf nano-*
+LOC=`grep "/nano-.*.t[gx]z$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
+wget -q $SLACKREPO/$LOC
+THENANO=$(ls nano-*)
+tar -xf $THENANO -C /boot/initrd-tree usr/bin/nano
+rm -f nano-*
+rm -f $THENANO 
+LOC=`grep "/file-.*.t[gx]z$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
+wget -q $SLACKREPO/$LOC
+THEFILE=$(ls file-*)
+tar -xf $THEFILE -C /boot/initrd-tree \
+  usr/lib$LIBDIRSUFFIX/libmagic.so.1.0.0
+rm -f $THEFILE
 }
-install_btrfs_progs
-
-install_httpfs2() {
-echo "Downloading httpfs2..."
-rm -f httpfs2-*.txz
-LOC=`grep "\/httpfs2-.*-.*-.*\.txz$" salix.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SALIXREPO/$LOC
-echo "Installing httpfs2..."
-spkg -qq --root=/boot/initrd-tree/ -i httpfs2-*.txz
-rm httpfs2-*.txz
-}
-#install_httpfs2
-
-install_cyrus_sasl() {
-echo "Downloading cyrus-sasl..."
-rm -f cyrus-sasl-*.txz
-LOC=`grep "\/cyrus-sasl-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SLACK2REPO/$LOC
-echo "Installing cyrus-sasl..."
-spkg -qq --root=/boot/initrd-tree/ -i cyrus-sasl-*.txz
-rm cyrus-sasl-*.txz
-}
-#install_cyrus_sasl
-
-install_samba() {
-echo "Downloading samba..."
-rm -f samba-*.txz
-LOC=`grep "\/samba-.*-.*-.*\.txz$" slack.md5 | sed "s|\(.*\)  \./\(.*\)|\2|"`
-wget -q $SLACK2REPO/$LOC
-echo "Installing smbclient binary from samba..."
-tar xf samba-*.txz -C /boot/initrd-tree --wildcards \
-  usr/bin/smbclient \
-  usr/lib${LIBDIRSUFFIX}/*.dat \
-  usr/lib${LIBDIRSUFFIX}/charset/*
-install -d /boot/initrd-tree/etc/samba
-touch /boot/initrd-tree/etc/samba/smb.conf
-rm samba-*.txz
-}
-#install_samba
+install_nano
 
 echo "Tweaking config files..."
 # network logon message
@@ -241,21 +245,26 @@ sed "/^127.0.0.1/s|slackware|salix${LIBDIRSUFFIX}|g" -i /boot/initrd-tree/etc/ho
 # remove not needed dirs
 echo "Removing unnecessary stuff..."
 rm -rf /boot/initrd-tree/usr/{doc,include,man,src}
-rm -rf /boot/initrd-tree/usr/share/locale
+# For localization, we need /boot/initrd-tree/usr/share/locale!
+# rm -rf /boot/initrd-tree/usr/share/locale
 rm -rf /boot/initrd-tree/usr/src
 rm -rf /boot/initrd-tree/var/log/packages
-# remove cyrus-sasl stuff
-rm -f /boot/initrd-tree/usr/sbin/{pluginviewer,saslauthd,sasldblistusers2,saslpasswd2,testsaslauthd}
-rm -f /boot/initrd-tree/etc/rc.d/rc.saslauthd
-rm -rf /boot/initrd-tree/usr/lib${LIBDIRSUFFIX}/sasl*/
+# remove cyrus-sasl stuff - No mre installed - Didier
+# rm -f /boot/initrd-tree/usr/sbin/{pluginviewer,saslauthd,sasldblistusers2,saslpasswd2,testsaslauthd}
+# rm -f /boot/initrd-tree/etc/rc.d/rc.saslauthd
+# rm -rf /boot/initrd-tree/usr/lib${LIBDIRSUFFIX}/sasl*/
 
 # in i486 the initrd is >32MB, so we need to split it in two
 if [ "$arch" == "x86_64" ] ; then
 	# repack x86_64 initrd
 	echo "Repacking x86_64 initrd..."
-	depmod -b /boot/initrd-tree/
+	# We didn't install modules, so no need for depmod - Didier
+	# depmod -b /boot/initrd-tree
 	rm -f /boot/initrd-tree/{wait-for-root,rootfs,rootdev,initrd-name}
-	mkinitrd -o $CWD/initrd/$arch/initrd.img
+	( cd /boot/initrd-tree
+	  find . -print | cpio -o --owner root:root -H newc \
+	  | gzip -9 > $CWD/initrd/$arch/initrd.img
+	)
 else
 	#
 	# first create a combined initrd
@@ -271,7 +280,7 @@ else
 	# first pack the non-smp initrd
 	echo "Repacking i486 non-smp initrd..."
 	rm -rf /boot/initrd-tree/lib/modules/*-smp
-	depmod -b /boot/initrd-tree/ $( uname -r | sed "s/-smp//" )
+	# depmod -b /boot/initrd-tree/ $( uname -r | sed "s/-smp//" )
 	rm -f /boot/initrd-tree/{wait-for-root,rootfs,rootdev,initrd-name}
 	mkinitrd -o $CWD/initrd/$arch/nosmp.img -k $( uname -r | sed "s/-smp//" )
 	# then pack the smp initrd
@@ -279,7 +288,7 @@ else
 	rm -rf /boot/initrd-tree
 	mv /boot/initrd-tree-copy /boot/initrd-tree
 	rm -rf $( ls -d /boot/initrd-tree/lib/modules/* | grep -v smp )
-	depmod -b /boot/initrd-tree/
+	# depmod -b /boot/initrd-tree/
 	rm -f /boot/initrd-tree/{wait-for-root,rootfs,rootdev,initrd-name}
 	mkinitrd -o $CWD/initrd/$arch/smp.img
 fi
